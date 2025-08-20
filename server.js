166766c1778c898b752efa9f74ef3df451373105
m7mdrf3t <m_rh50@yahoo.com>
2025-08-20 03:45:38 +0300

    Fix Express.js dependencies and node-fetch import for Railway deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
